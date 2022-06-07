b4078d933065203c266cca8677c5168620adec88
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-07 18:51:29 +0300

    add new expense and update folder structure

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,23 @@
+import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
-import Card from './Card';
+import Card from '../UI/Card';
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const handleClick = () => {
+    console.log('Clicked');
+    setTitle('New Title');
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={handleClick}>Change Title</button>
       </div>
     </Card>
   );
