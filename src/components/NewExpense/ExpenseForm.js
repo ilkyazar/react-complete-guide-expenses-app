eb1f9bc33f6f8d52ba737a35e2f6406689ef76d5
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-07 19:17:44 +0300

    add multiple states to expense form and handle submit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,59 @@
+import { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  const handleTitleChange = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const handleAmountChange = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log('Expense Data: ', expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={handleTitleChange} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={handleAmountChange}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={handleDateChange}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
